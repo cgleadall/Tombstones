fab766ba72fa0033af8b14f96300e0589ac21fb8
CGleadall <gleadall@hotmail.com>
2012-08-24 09:59:53 -0600

    Shell of new website

---
+++ Tombstones.UI.Web/Tombstones.UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tombstones.UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "We have moved...";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "We provide geneological research assistance for the Caribbean.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "If you have any questions...";
+
+            return View();
+        }
+    }
+}
