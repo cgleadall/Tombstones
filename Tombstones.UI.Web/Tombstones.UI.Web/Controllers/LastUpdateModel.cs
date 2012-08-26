18dc58bda1c3f35e5d836dc12035bceebba68242
CGleadall <gleadall@hotmail.com>
2012-08-26 14:18:32 -0600

    added a user model object

---
+++ Tombstones.UI.Web/Tombstones.UI.Web/Controllers/LastUpdateModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tombstones.UI.Web.Models
+{
+    public class LastUpdateModel
+    {
+
+        public DateTime LastUpdateTime { get; set; }
+    }
+}
---
+++ Tombstones.UI.Web/Tombstones.UI.Web/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tombstones.UI.Web.Models
+{
+    public class HomeModel
+    {
+        [Display(Name = "Last Updated:")]
+        public DateTime LastUpdateTime { get; set; }
+    }
+}
