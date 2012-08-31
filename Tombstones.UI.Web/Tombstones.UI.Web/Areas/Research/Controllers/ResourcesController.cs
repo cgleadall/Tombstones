6e28e2eb49835bb8424de3d13d91cbedec76a0a2
CGleadall <gleadall@hotmail.com>
2012-08-31 15:43:04 -0600

    added Research area and model class for research resources.

---
+++ Tombstones.UI.Web/Tombstones.UI.Web/Areas/Research/Controllers/ResourcesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tombstones.UI.Web.Areas.Research.Controllers
+{
+    public class ResourcesController : Controller
+    {
+
+        public static string ResourcesResearchLinkPath = "/research/resources/all";
+        //
+        // GET: /Research/Resources/
+
+        public ActionResult All()
+        {
+            ViewBag.Title = "Research Resources";
+            return View();
+        }
+
+        public ActionResult Index(string id)
+        {
+            var country = id;
+            ViewBag.Title = "Research Resouces";
+            ViewBag.Country = country;
+
+            var model = new Models.ResourceCollection();
+            
+            return View(model);
+        }
+
+    }
+}
---
+++ Tombstones.UI.Web/Tombstones.UI.Web/Areas/Research/Models/Resource.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tombstones.UI.Web.Areas.Research.Models
+{
+    public class Resource
+    {
+        public string Country { get; set; }
+        public string Title { get; set; }
+        public IList<string> Addresses { get; set; }
+
+        public Resource()
+        {
+            Addresses = new List<string>();
+        }
+    }
+
+    public class ResourceCollection
+    {
+        public IList<Resource> Resources { get; set; }
+
+        public ResourceCollection()
+        {
+            Resources = new List<Resource>();
+
+            SeedCollection();
+        }
+
+        private void SeedCollection()
+        {
+            // Antigua
+            var item = new Resource{
+                Country = "Antigua",
+                Title = "Antigua & Barbuda National Archives"
+            };
+            item.Addresses.Add("Victoria Park, Factory Road, St. John's, Antigua");
+            item.Addresses.Add(">1-268- 462 -3946 / 7");
+            item.Addresses.Add("archives@candw.ag");
+
+            Resources.Add(item);
+        }
+    }
+}
