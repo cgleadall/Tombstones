a80727d7c2b9b12eda5fc24fd3e64c0e939034c5
CGleadall <gleadall@hotmail.com>
2012-09-09 15:38:20 -0600

    added Undo Import so you can delete records that were imported.

---
+++ Tombstones.UI.Web/Tombstones.UI.Web/ViewModels/FileManagerUndoImport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tombstones.UI.Web.ViewModels
+{
+    public class FileManagerUndoImport
+    {
+        public string UploadedFileId { get; set; }
+        public bool ConfirmRemoval { get; set; }
+        public int RemainingRecords { get; set; }
+        public int BatchSize { get; set; }
+        public TimeSpan ExecutionTimeSpan { get; set; }
+
+        public FileManagerUndoImport()
+        {
+            BatchSize = 100;
+        }
+        public static FileManagerUndoImport Create(string uploadedFileId)
+        {
+            var result = new FileManagerUndoImport();
+            result.UploadedFileId = uploadedFileId;
+            return result;
+        }
+    }
+}
